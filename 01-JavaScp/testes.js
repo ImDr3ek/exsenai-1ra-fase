92fa379eb9d65cce89bc930207d4a738cf427e0d
ImDr3ek <ggdiego581@gmail.com>
2026-04-16 22:13:42 -0300

    js

---
+++ 01-JavaScp/testes.js
@@ -0,0 +1,18 @@
+//alert("Ola Gente bizarra")
+//alert("SEGUNDA MENSAGEM")
+let nome
+
+
+nome = "Pipoca azul"
+
+//alert("Maiora das pipocas sao brancas, ou amarelas, mesmo sendo a maioria, a melhor sempre e a " + nome)
+
+let idade = 30
+
+//nome = prompt("Digite sua pipoca favorita\n" )
+//alert("qual e a pipoca??:" + nome)
+let nota1
+nota1 = prompt("Nota do site: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
