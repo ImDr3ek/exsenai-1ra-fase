c4ee96a2e0f1ee4f989eb01488cfddd2127ee769
ImDr3ek <ggdiego581@gmail.com>
2026-04-23 21:49:05 -0300

    Mudanças de Tudo

--- 01-JavaScp/testes.js
+++ 01-JavaScp/testes.js
@@ -1,18 +1,41 @@
 //alert("Ola Gente bizarra")
 //alert("SEGUNDA MENSAGEM")
-let nome
+//let nome
 
 
-nome = "Pipoca azul"
+//nome = "Pipoca azul"
 
 //alert("Maiora das pipocas sao brancas, ou amarelas, mesmo sendo a maioria, a melhor sempre e a " + nome)
 
-let idade = 30
+//let idade = 30
 
 //nome = prompt("Digite sua pipoca favorita\n" )
 //alert("qual e a pipoca??:" + nome)
-let nota1
-nota1 = prompt("Nota do site: ")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+//let nota1
+//nota1 = prompt("Nota do site: ")
+//nota1 = Number(nota1)
+//nota1 = nota1 + 1
+//alert(nota1)
+//..........................
+//let vitoria, empate 
+//let pontos
+//vitoria =prompt("Numero de Vitorias")
+//vitoria =Number(vitoria)
+//empate =prompt("Numero de empates")
+//empate =Number(empate)
+//empate =(empate)
+//pontos =(vitoria * 3) + empate
+//alert("pontos totais: "+ pontos)
+//............................
+let preco, quantidade
+let valortotal
+quantidade = Number(prompt("Quantidade:"))
+preco = Number(prompt("Preço:"))
+
+
+console.log("Valor a receber:")
+
+
+
+
+
