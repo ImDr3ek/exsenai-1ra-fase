5f8279d82036cc75a29c64aa3bf2618200622e62
ImDr3ek <ggdiego581@gmail.com>
2026-04-23 22:35:24 -0300

    Linkin park

--- 01-JavaScp/testes.js
+++ 01-JavaScp/testes.js
@@ -27,13 +27,23 @@
 //pontos =(vitoria * 3) + empate
 //alert("pontos totais: "+ pontos)
 //............................
-let preco, quantidade
-let valortotal
-quantidade = Number(prompt("Quantidade:"))
-preco = Number(prompt("Preço:"))
+//let preco, quantidade
+//let valortotal
+//quantidade = Number(prompt("Quantidade:"))
+//preco = Number(prompt("Preço:"))
+//valortotal = quantidade * preco
+//console.log("Valor a receber:" + valortotal.toFixed(2))
+//......
+let nome, resposta
+nome = prompt("QUal seu nome: ")
+resposta = prompt("Voce gosta do linkin park?: ")
+resposta.toUpperCase
+if(resposta!="SIM"){
+window.location.href= "https://www.google.com/search?rlz=1C1GCHN_pt-BRBR1207BR1208&cs=0&sca_esv=1cd8785de84224e8&sxsrf=ANbL-n42p9D8IFnOk0aAnAj49g5JGHgUIQ:1776986979042&udm=2&fbs=ADc_l-bpk8W4E-qsVlOvbGJcDwpn60DczFdcvPnuv8WQohHLTaMb_WtLz8zQ41bNqiqMK_0GCDA2eBSrpJajLJh54y7KaCHLPvoYcuFrrCOzk_nugrV4iIWfPa091uWzhe1c92-JzF-rN68BLKtfLAyu3BJu7h4s0gVaklPerkyVM83e84uGwH1ulfguqBOOfRuJJCY4EsZdnbfBWJGjulqj5odSFoSEQKGQl6CTwdYSr-gxd-x2on4&q=linkin+park+icon&sa=X&ved=2ahUKEwjD55-tkIWUAxU3NrkGHXF7FTkQtKgLegQIFRAB&biw=1600&bih=765&dpr=1#sv=CAMSVhoyKhBlLXFoSXE5TXA5aWs3QjFNMg5xaElxOU1wOWlrN0IxTToOcS1NQTd4dW9SVF8wbU0gBCocCgZtb3NhaWMSEGUtcWhJcTlNcDlpazdCMU0YADABGAcgjvulwwJKCBACGAEgAigB"
+}else{
+    alert("Boa paizao")
+}
 
-
-console.log("Valor a receber:")
 
 
 
