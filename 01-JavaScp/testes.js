eb7c8be00ed52f6c01c094947a2acad231945dce
ImDr3ek <ggdiego581@gmail.com>
2026-04-30 22:14:53 -0300

    Obrigado Eu

--- 01-JavaScp/testes.js
+++ 01-JavaScp/testes.js
@@ -93,7 +93,59 @@ function suruman(){
         document.getElementById("resultado").innerHTML = "Fumaça bloqueada. Aguardar condições ideais."
     }
 }
+function surumandos(){
+    let estaGripado, tempoViagem
+    
+    estaGripado = confirm("Você está gripado? (ok: sim, cancelar: não)")
+    tempoViagem = Number(prompt("Quanto tempo de viagem? (tempo em minutos)"))
+    
+    if(estaGripado == true || tempoViagem > 45){
+        document.getElementById("resultado").innerHTML = "Deslocamento inviável. Melhor recusar este evento."
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "Viagem autorizada. Rumo ao show!"
+    }
+}
 
+function surumantres(){
+    let somFuncionando, figurinoCompleto
+    
+    somFuncionando = confirm("O som está funcionando? (ok: sim, cancelar: não)")
+    somFuncionando = confirm("O figurino está completo? (ok: sim, cancelar: não)")
+    
+    if(somFuncionando == true && figurinoCompleto == true){
+        document.getElementById("resultado").innerHTML = "Palco liberado para Sarumano! Luzes, câmera, ilusão!"
+    }
+    else{if(somFuncionando == false && figurinoCompleto == true){
+        document.getElementById("resultado").innerHTML = "Sem som. Realizar apresentação alternativa no salão."
+    }
+    else{if(somFuncionando == true && figurinoCompleto == false){
+        document.getElementById("resultado").innerHTML = "Faltando parte do figurino. Te vira no improviso!"
+        }
+        else{
+                 document.getElementById("resultado").innerHTML = "Apresentação cancelada. Hora da mágica de desaparecer…"
+                }
+            }
+        }
+    }
+    
+    function dennyssIA(){
+        let emManutencao, emergenciaAtiva
+        
+        emManutencao = confirm("O parque está em manutenção? (ok: sim, cancelar: não)")
+        emergenciaAtiva = confirm("O parque está com alerta de emergência? (ok: sim, cancelar: não)")
+        
+        if(emManutencao == true || emergenciaAtiva == true){
+            document.getElementById("resultado").innerHTML = "Transporte indisponível por motivo de segurança."
+        }
+        else{
+            document.getElementById("resultado").innerHTML = "Transporte liberado para uso."
+        }
+    }
+
+    function dennyss(){
+        
+    }
 
 
 
